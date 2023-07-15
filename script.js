4b19e6d65236f12a3a1e4c08a750d1930c6a0ae6
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-07-16 01:23:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 const marquee = document.querySelectorAll('.marquee__content');
 
-// Get the width of the details container
-const detailsWidth = document.querySelector('.details').offsetWidth;
-
-// Set the left margin of the last list item to the width of the details container
-
-for (let i = 0; i < marquee.length; i++) {
-  marquee[i].lastElementChild.style.marginRight = `${detailsWidth}px`;
-}
-
 // Pause both marquee__content elements when hovering over an li element
 const liElements = document.querySelectorAll('.marquee__content li');
+
 liElements.forEach(li => {
   li.addEventListener('mouseenter', () => {
-    for (let i = 0; i < marquee.length; i++) {
-   
 
+    for (let i = 0; i < marquee.length; i++) {
       marquee[i].style.animationPlayState = 'paused';
     }
   });
@@ -25,20 +16,19 @@ liElements.forEach(li => {
     }
   });
 });
-
-
 // Get the position of the .details element
 const details = document.querySelectorAll('.details');
-
-
 setInterval(() => {
   details.forEach((detail) => {
     const detailPosition = detail.getBoundingClientRect();
-    console.log(detailPosition.left)
 
     if (detailPosition.left <= -50) {
-      detail.style.marginLeft = "30rem";
-    } 
-    
+      detail.style.marginLeft = `30rem`;
+    }
+    if (detailPosition.left >= 400 ) {
+      detail.style.marginLeft = `0px`;
+    }
+  
+
   });
-}, 10);
+}, 1);
